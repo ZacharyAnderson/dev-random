d34746524458e1e96b11daa745b87db3886a971d
ZacharyAnderson <zacha777@gmail.com>
2019-05-29 13:36:02 -0400

    have a working solution

--- dev_random.py
+++ dev_random.py
@@ -1,5 +1,6 @@
 import os
 import time
+import hashlib
 
 
 def seed_generator():
@@ -7,5 +8,35 @@ def seed_generator():
         str(time.perf_counter())
 
 
+def hash_generator(entropy, seed):
+    print(hashlib.sha256((seed + entropy).encode('utf-8')).hexdigest())
+
+
+def generate_entropy():
+    string = ''
+    list = os.listdir('/var/log/')
+    for path in list:
+        if path.find('.log') != -1:
+            with open('/var/log/'+path, 'rb') as file:
+                string += str(file.read().hex())
+    return string
+
+
+def dev_random(limit):
+    random_count = 0
+    seed = seed_generator()
+    entropy = generate_entropy()
+    max_entropy = len(entropy)//64
+    while random_count < limit:
+        if random_count == 0:
+            hash_generator(entropy[random_count:random_count+64], seed)
+        else:
+            hash_generator(entropy[random_count*64:(random_count*64)+64], seed)
+        random_count += 1
+        if random_count >= max_entropy:
+            seed = seed_generator()
+
+
 if __name__ == "__main__":
-    print(seed_generator())
+
+    dev_random(24)
