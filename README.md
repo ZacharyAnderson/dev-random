77d3ac4bd9168d2f36109944a8beb62df94737a6
ZacharyAnderson <zacha777@gmail.com>
2019-05-29 14:44:53 -0400

    Added comments

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Python implmentation of /dev/random
--- dev_random.py
+++ dev_random.py
@@ -1,18 +1,34 @@
 import os
 import time
 import hashlib
+import argparse
 
 
 def seed_generator():
+    """
+        seed_generator takes the time, the python process id,
+        the current proccess's effective user id, the time
+        the python program has been running and creates a string.
+    """
     return str(time.time()) + str(os.getpid()) + str(os.geteuid()) +\
         str(time.perf_counter())
 
 
 def hash_generator(entropy, seed):
+    """
+        hash_generator takes a 64 char string from the entropy pool
+        and adds that with our seed. Then it creats a sha256 hash
+        and prints a hexidecimal number.
+    """
     print(hashlib.sha256((seed + entropy).encode('utf-8')).hexdigest())
 
 
 def generate_entropy():
+    """
+        generate_entropy will create our entropy pool. To do this we read
+        all available log files from /var/log/ and create one large string
+        of data to be read from.
+    """
     string = ''
     list = os.listdir('/var/log/')
     for path in list:
@@ -23,6 +39,15 @@ def generate_entropy():
 
 
 def dev_random(limit):
+    """
+        dev_random takes an input - limit which is the amount of hashes to
+        be generated. First, we create our seed and generate our entropy pool.
+        Then, we start creating our hashes by working down our entropy pool in
+        64 character blocks and inputing that with our seed to generate our hashs.
+        In the chance the limit is greater then the entropy pool we will recursively
+        call the dev_random function with the limit being the remaining hashes and
+        regenerate the entropy and seeds.
+    """
     random_count = 0
     seed = seed_generator()
     entropy = generate_entropy()
@@ -33,10 +58,19 @@ def dev_random(limit):
         else:
             hash_generator(entropy[random_count*64:(random_count*64)+64], seed)
         random_count += 1
+
         if random_count >= max_entropy:
-            seed = seed_generator()
+            dev_random(limit - max_entropy)
+            return
 
 
 if __name__ == "__main__":
 
-    dev_random(24)
+    parser = argparse.ArgumentParser(
+        description="Generate random number hashes.")
+    parser.add_argument(
+        '--limit', "-L", help="Amount of hashes required. Must be a positive integer",
+        required=True, type=int)
+    args = vars(parser.parse_args())
+
+    dev_random(args['limit'])
